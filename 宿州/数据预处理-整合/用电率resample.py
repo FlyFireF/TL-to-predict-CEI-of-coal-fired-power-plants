febc2d3a520956fd910b3008e9b68f94561bb0e5
FlyFireF <971129603@qq.com>
2025-09-09 22:03:18 +0800

    add

--- 宿州/数据预处理-整合/用电率resample.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import os
-file_path=r"D:\FlyFireF\Work and Study\毕业设计\宿州电厂数据汇总\5-6厂用电率\用电率.xlsx"
-df = pd.read_excel(file_path)
-df['时间'] = pd.to_datetime(df['时间'])
-df.set_index('时间', inplace=True)
-daily = df.resample('d').mean()
-daily.to_excel("data/宿州电厂日平均用电率.xlsx")
